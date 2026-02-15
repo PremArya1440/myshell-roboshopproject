cc8e1e724446c7fb6f33555ebf2f8db23a7bacf3
prem DevOps Engineer <premdevops@gmail.com>
2026-02-15 23:54:53 +0530

    IP address print

--- 01-security-group.sh
+++ 01-security-group.sh
@@ -4,6 +4,7 @@ SG_ID="sg-08ccf1ff6b7744e3c"   #replace  with your security group id
 AMI_ID="ami-0c55b159cbfafe1f0"   #replace with your AMI id
 ZONE_ID="Z0425694W58WDQOZHX5V"   #replace with your availability zone
 DOMAIN_NAME="4sinfotech.in"   #replace with your domain name
+
 for instance in $@
 do
 # aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-08ccf1ff6b7744e3c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" 
