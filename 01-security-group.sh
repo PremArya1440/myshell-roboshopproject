d39620d95801c6205f63763cde5853877f9c9b97
prem DevOps Engineer <premdevops@gmail.com>
2026-02-16 11:48:18 +0530

    priorities for mongodb

--- 01-security-group.sh
+++ 01-security-group.sh
@@ -9,7 +9,7 @@ for instance in $@
 do
 # aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-08ccf1ff6b7744e3c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" 
 
-$INSTANCE_ID= 
+INSTANCE_ID= 
 
 $(aws ec2 run-instances \
 --image-id $AMI_ID \ 
@@ -18,7 +18,7 @@ $(aws ec2 run-instances \
 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
 --query 'Instances[0].InstanceId' \
 --output text ) 
-if [ --instance == "frontend" ]; then
+if [ $instance == "frontend" ]; then
    IP=$(   
       aws ec2 describe-instances \
        --instance-ids $INSTANCE_ID \
