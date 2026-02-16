fd66c8d601170e9f1a50f8bdde06ed08909db143
prem DevOps Engineer <premdevops@gmail.com>
2026-02-16 12:01:08 +0530

    priorities for mongodb

--- 01-security-group.sh
+++ 01-security-group.sh
@@ -7,32 +7,57 @@ DOMAIN_NAME="4sinfotech.in"   #replace with your domain name
 
 for instance in $@
 do
-# aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-08ccf1ff6b7744e3c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" 
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
 
-INSTANCE_ID= 
-
-$(aws ec2 run-instances \
---image-id $AMI_ID \ 
---instance-type t3.micro \
---security-group-ids $SG_ID \
---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
---query 'Instances[0].InstanceId' \
---output text ) 
-if [ $instance == "frontend" ]; then
-   IP=$(   
-      aws ec2 describe-instances \
-       --instance-ids $INSTANCE_ID \
-       --query 'Reservations[].Instances[].PublicIpAddress' \
-        --output text
-   )
-   else
-    IP=$(
-        aws ec2 describe-instances \
-         --instance-ids $INSTANCE_ID \
-         --query 'Reservations[].Instances[].PrivateIpAddress' \
-          --output text
-    )
-
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
     fi
+
     echo "IP Address: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    '
+
+    echo "record updated for $instance"
+
 done
